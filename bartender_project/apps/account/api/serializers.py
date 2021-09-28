5f73c5486a72039e3df5c4a0e3a9db3263e733d2
MaksimMarynau <oliverknightbel@gmail.com>
2021-09-28 15:53:54 +0200

    added pass2 to userserializer

--- bartender_project/apps/account/api/serializers.py
+++ bartender_project/apps/account/api/serializers.py
@@ -6,13 +6,20 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
 
+    password2 = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True,
+        min_length=4,
+        max_length=16,
+        help_text='Passwords must match.'
+    )
     account_type = serializers.StringRelatedField(read_only=True)
     user_cocktails = serializers.StringRelatedField(many=True, read_only=True)
     user_ingredients = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = get_user_model()
-        fields = ('id','username','first_name','last_name','password','user_cocktails','user_ingredients','account_type')
+        fields = ('id','username','email','first_name','last_name','password','password2','user_cocktails','user_ingredients','account_type')
 
         extra_kwargs = {
             'password': {
@@ -26,6 +33,12 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match.'})
+        validated_data.pop('password2')
+        
         return get_user_model().objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
